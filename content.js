c50e046bc22cc4090fffd4ae6c85856d102114f0
VictorNguyenLPN <quanghuy71847@gmail.com>
2025-08-27 22:16:09 +0700

    Simplify source code

--- content.js
+++ content.js
@@ -1,26 +1,3 @@
-// import {
-//   PERIOD_REGULATIONS,
-//   SEMESTER_START,
-//   WEEKLY_SCHEDULE_TABLE_SELECTOR,
-//   GENERAL_SCHEDULE_TABLE_SELECTOR,
-//   DAY_OF_WEEK,
-//   PROCESSED_SCHEDULE,
-// } from "./config";
-// import { parseCell } from "./helper";
-
-function parseCell(cell) {
-  const spans = cell.querySelectorAll("span");
-  const subjects = [];
-  spans.forEach((span) => {
-    const p = span.querySelector("p");
-    if (!p) return;
-    const title = p.querySelector("b")?.childNodes[0]?.textContent.trim();
-    const details = p.innerText.trim();
-    subjects.push({ title, details });
-  });
-  return subjects;
-}
-
 const WEEKLY_SCHEDULE_TABLE_SELECTOR = "ThoiKhoaBieu1_tbTKBTheoTuan";
 const GENERAL_SCHEDULE_TABLE_SELECTOR = "ThoiKhoaBieu1_Table1";
 const DAY_OF_WEEK = [
@@ -45,6 +22,19 @@ const PROCESSED_SCHEDULE = {
   Saturday: { Morning: {}, Afternoon: {}, Evening: {} },
   Sunday: { Morning: {}, Afternoon: {}, Evening: {} },
 };
+
+function parseCell(cell) {
+  const spans = cell.querySelectorAll("span");
+  const subjects = [];
+  spans.forEach((span) => {
+    const p = span.querySelector("p");
+    if (!p) return;
+    const title = p.querySelector("b")?.childNodes[0]?.textContent.trim();
+    const details = p.innerText.trim();
+    subjects.push({ title, details });
+  });
+  return subjects;
+}
 
 function parseCell(cell) {
   const spans = cell.querySelectorAll("span");
