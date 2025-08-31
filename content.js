d68778e4dfd483458f27d8bdb16feec49b60fc30
VictorNguyenLPN <quanghuy71847@gmail.com>
2025-08-31 20:14:14 +0700

    Ready to convert -> ics format (fully processed raw schedule)

--- content.js
+++ content.js
@@ -8,25 +8,81 @@ const DAY_OF_WEEK = [
   "Sunday",
 ];
 
-const PERIOD_TIME = {
-  1: "06:50 - 07:40",
-  2: "07:40 - 08:30",
-  3: "08:30 - 09:20",
-  4: "09:30 - 10:20",
-  5: "10:20 - 11:10",
-  6: "11:10 - 12:00",
-  7: "12:45 - 13:35",
-  8: "13:35 - 14:25",
-  9: "14:25 - 15:15",
-  10: "15:25 - 16:15",
-  11: "16:15 - 17:05",
-  12: "17:05 - 17:55",
-  13: "18:05 - 18:55",
-  14: "18:55 - 19:45",
-  15: "19:45 - 20:35",
+const DAY_MAP = {
+  Monday: 1,
+  Tuesday: 2,
+  Wednesday: 3,
+  Thursday: 4,
+  Friday: 5,
+  Saturday: 6,
+  Sunday: 0,
 };
 
-function parseCell(cell) {
+const PERIOD_TIME = {
+  1: { startHour: 6, startMinute: 50, endHour: 7, endMinute: 40 },
+  2: { startHour: 7, startMinute: 40, endHour: 8, endMinute: 30 },
+  3: { startHour: 8, startMinute: 30, endHour: 9, endMinute: 20 },
+
+  4: { startHour: 9, startMinute: 30, endHour: 7, endMinute: 40 },
+  5: { startHour: 10, startMinute: 20, endHour: 11, endMinute: 10 },
+  6: { startHour: 11, startMinute: 10, endHour: 12, endMinute: 0 },
+
+  7: { startHour: 12, startMinute: 45, endHour: 113, endMinute: 35 },
+  8: { startHour: 13, startMinute: 35, endHour: 14, endMinute: 25 },
+  9: { startHour: 14, startMinute: 25, endHour: 15, endMinute: 15 },
+
+  10: { startHour: 15, startMinute: 25, endHour: 16, endMinute: 15 },
+  11: { startHour: 16, startMinute: 15, endHour: 17, endMinute: 5 },
+  12: { startHour: 17, startMinute: 5, endHour: 17, endMinute: 55 },
+
+  13: { startHour: 18, startMinute: 5, endHour: 18, endMinute: 55 },
+  14: { startHour: 18, startMinute: 55, endHour: 19, endMinute: 45 },
+  15: { startHour: 19, startMinute: 50, endHour: 20, endMinute: 35 },
+};
+
+function getTimeRange(periodString) {
+  const arrString = periodString.split("");
+  const arrNumbers = arrString.map(Number);
+
+  const timeRange = {
+    startHour: PERIOD_TIME[arrNumbers[0]].startHour,
+    startMinute: PERIOD_TIME[arrNumbers[0]].startMinute,
+    endHour: PERIOD_TIME[arrNumbers[arrNumbers.length - 1]].endHour,
+    endMinute: PERIOD_TIME[arrNumbers[arrNumbers.length - 1]].endMinute,
+  };
+
+  return timeRange;
+}
+
+function parseWeeks(weekString) {
+  const weeks = [];
+  for (let i = 0; i < weekString.length; i++) {
+    const ch = weekString[i];
+    if (ch === "-") continue;
+    if (i < 9) {
+      weeks.push(i + 1);
+    } else {
+      weeks.push(10 + parseInt(ch));
+    }
+  }
+  return weeks;
+}
+
+function getCourseDates(week, startDateStr, weekday) {
+  const startDate = new Date(startDateStr);
+  const weeks = parseWeeks(week);
+  const dates = [];
+
+  weeks.forEach((week) => {
+    const date = new Date(startDate);
+    date.setDate(date.getDate() + (week - 1) * 7 + weekday);
+    dates.push(date.toISOString().split("T")[0]);
+  });
+
+  return dates;
+}
+
+function parseCell(cell, weekDay) {
   const spans = cell.querySelectorAll("span");
   const subjects = [];
 
@@ -60,6 +116,8 @@ function parseCell(cell) {
       period,
       room,
       week,
+      dateList: getCourseDates(week, "2025-08-10", weekDay),
+      timeRange: getTimeRange(period),
     });
   });
 
@@ -82,7 +140,7 @@ function extractSchedule() {
     const cells = row.querySelectorAll("td.cell");
     cells.forEach((cell, cellIndex) => {
       const weekday = DAY_OF_WEEK[cellIndex];
-      const courses = parseCell(cell, weekday);
+      const courses = parseCell(cell, cellIndex + 1);
 
       if (courses.length > 0) {
         results[weekday][period].push(...courses);
@@ -93,5 +151,5 @@ function extractSchedule() {
   return results;
 }
 
-const res = extractSchedule();
-console.log(res);
+const tmp = extractSchedule();
+console.log(tmp);
